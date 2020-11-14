c83cd38a4c8622701414bd191b64af607a9cfb9d
Marta Pancaldi <marta.panc@gmail.com>
2020-11-14 20:43:31 +0000

    Add method to search cadastral code in page

--- TownCodeScraper.py
+++ TownCodeScraper.py
@@ -27,8 +27,28 @@ def main():
             while i < len(links) and not "cite_note" in links[i]['href'] and not links[i]['title'].isnumeric():
                 target_town_pages.append(base_url + links[i]['href'])
                 i = i+1
+
+        search_cadastral_code_in_pages(source_town_pages)
+        search_cadastral_code_in_pages(target_town_pages)
+
         print(links)
 
 
 if __name__ == '__main__':
     main()
+
+
+def search_cadastral_code_in_pages(page_list):
+    for url in page_list:
+        if not "redlink" in url:
+            resp = requests.get(url=url)
+            page_soup = BeautifulSoup(resp.content, 'html.parser')
+            tr_list = page_soup.find_all('tr')
+
+            td = page_soup.select('th')
+            for name, val in zip(td, td[1:]):
+                a = name.find_next('a')
+                if a.has_attr('title') and "catastale" in a['title']:
+                    code = name.parent.find_next('td').text.replace('\n', '')
+                    print(code)
+                    break
