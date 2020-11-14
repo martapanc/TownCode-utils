d6e01d3d7583f1a86b32a2381cc9c2366aa9156f
Marta Pancaldi <marta.panc@gmail.com>
2020-11-14 21:19:02 +0000

    Fix search method

--- TownCodeScraper.py
+++ TownCodeScraper.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+def search_cadastral_code_in_pages(page_list):
+    for url in page_list:
+        if not "redlink" in url:
+            resp = requests.get(url=url)
+            page_soup = BeautifulSoup(resp.content, 'html.parser')
+            tr_list = page_soup.find_all('tr')
+
+            td = page_soup.select('th')
+            for name, val in zip(td, td[1:]):
+                a = name.find_next('a')
+                if a.has_attr('title') and "catastale" in a['title']:
+                    code = name.parent.find_next('td').text.replace('\n', '')
+                    print(code)
+                    break
 
 def main():
+    min_year = 1930
     base_url = "https://it.wikipedia.org"
-
-    response = requests.get(
-        url="https://it.wikipedia.org/wiki/Comuni_d%27Italia_soppressi"
-    )
 
     # soup = BeautifulSoup(response.content, 'html.parser')
     soup = BeautifulSoup(open("comunisoppressi.html"), "html.parser")
@@ -23,32 +34,18 @@ def main():
             i = i+1
         if i < len(links):
             year = links[i]['title']
-            i = i+1
-            while i < len(links) and not "cite_note" in links[i]['href'] and not links[i]['title'].isnumeric():
-                target_town_pages.append(base_url + links[i]['href'])
+            if int(year) >= min_year:
                 i = i+1
+                while i < len(links) and not "cite_note" in links[i]['href'] and not links[i]['title'].isnumeric():
+                    target_town_pages.append(base_url + links[i]['href'])
+                    i = i+1
 
-        search_cadastral_code_in_pages(source_town_pages)
-        search_cadastral_code_in_pages(target_town_pages)
+                search_cadastral_code_in_pages(source_town_pages)
+                search_cadastral_code_in_pages(target_town_pages)
 
-        print(links)
+                print(links)
 
 
 if __name__ == '__main__':
     main()
 
-
-def search_cadastral_code_in_pages(page_list):
-    for url in page_list:
-        if not "redlink" in url:
-            resp = requests.get(url=url)
-            page_soup = BeautifulSoup(resp.content, 'html.parser')
-            tr_list = page_soup.find_all('tr')
-
-            td = page_soup.select('th')
-            for name, val in zip(td, td[1:]):
-                a = name.find_next('a')
-                if a.has_attr('title') and "catastale" in a['title']:
-                    code = name.parent.find_next('td').text.replace('\n', '')
-                    print(code)
-                    break
