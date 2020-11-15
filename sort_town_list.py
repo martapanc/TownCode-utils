32a24856040cb84d7e5c1be466f4ba289b129592
Marta Pancaldi <marta.panc@gmail.com>
2020-11-15 21:35:09 +0000

    Add method to sort town list by name

--- sort_town_list.py
+++ sort_town_list.py
@@ -1,1 +1,14 @@
+import json
 
+
+def read_json():
+    with open('files/comuni.json') as json_file:
+        town_list = json.load(json_file)['towns']
+        town_list.sort(key=lambda s: s['id'])
+
+        with open('files/comuni_sorted.json', 'w') as output:
+            json.dump({"towns": town_list}, output, indent=4)
+
+
+if __name__ == '__main__':
+    read_json()
