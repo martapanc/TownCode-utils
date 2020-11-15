e616e442b445b05cf2f5b80bf7542fa4db6c1b1f
Marta Pancaldi <marta.panc@gmail.com>
2020-11-15 22:01:53 +0000

    Add method to remove duplicates based on town name

--- sort_town_list.py
+++ sort_town_list.py
@@ -6,9 +6,19 @@ def read_json():
         town_list = json.load(json_file)['towns']
         town_list.sort(key=lambda s: s['id'])
 
-        with open('files/comuni_sorted.json', 'w') as output:
-            json.dump({"towns": town_list}, output, indent=4)
+        write_json(town_list)
+
+
+def write_json(town_list):
+    with open('files/comuni_sorted.json', 'w') as output:
+        json.dump({"towns": town_list}, output, indent=4)
+
+
+def remove_duplicates(town_list):
+    unique = {each["id"]: each for each in town_list}.values()
+    print(unique)
 
 
 if __name__ == '__main__':
-    read_json()
+    # read_json()
+    remove_duplicates([{"id": "ciao", "cc": 1}, {"id": "ciao", "cc": 2}])
